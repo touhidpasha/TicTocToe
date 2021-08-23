1ce6c37eb967fd9f05f9c153f79793e3c292617f
touhidpasha <touhidpasha552@gmail.com>
2021-08-23 11:56:09 +0530

    methos for choosing between x and o added

--- TicTocToeGame.java
+++ TicTocToeGame.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 class Game{
     /*here Game class can we used to call methods by 
     creating it's objects*/
 
+    Scanner sc=new Scanner(System.in);
     public char[] board=new char[10];
+    char userChoice;    
     
     public void intialize(){
         for(int i=1;i<board.length;i++)
@@ -10,13 +14,21 @@ class Game{
                board[i]=' ';        //intializing array with spaces
             }                   
     }
+
+    public void selectOption()
+    {
+        System.out.println("Enter x or o as your choice");
+        userChoice=sc.next().charAt(0);
+        
+    }
     
     
 }
 
 public class TicTocToeGame{
     public static void main(String args[]){
-
-
+        Game obj=new Game();
+        obj.intialize();
+        obj.selectOption();
     }
 }
