f6cb4ca58cb84479b2c3642da2bd943b22f37436
touhidpasha <touhidpasha552@gmail.com>
2021-08-23 08:52:48 +0530

    UC1 done

---
+++ TicTocToeGame.java
@@ -0,0 +1,19 @@
+class Game{
+
+    public char[] board=new char[10];
+    
+    public void addToBoard(int x){
+
+    }
+    public int checkWinner(){
+        return(0);
+    }
+    
+}
+
+public class TicTocToeGame{
+    public static void main(String args[]){
+
+
+    }
+}
